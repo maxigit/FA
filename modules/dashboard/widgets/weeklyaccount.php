3bce90c91f6c0a7239737f448675dd0b01e04daf
Maxime Bourget <bmx007@gmail.com>
2013-03-17 19:41:54 +0000

    Add invert option

--- modules/dashboard/widgets/weeklyaccount.php
+++ modules/dashboard/widgets/weeklyaccount.php
@@ -16,8 +16,10 @@ class weeklyaccount
 
     var $weeks_past;
     var $weeks_future;
-    var $bank_act;
+    var $gl_accounts;
+    var $convert;
     var $graph_type;
+    var $display_total;
 function week_of_month($date) {
 	if ($this->by_month) return 1;
 	$next_date = add_months($date, 1);
@@ -31,13 +33,15 @@ function week_of_month($date) {
 	{
 	    $data=json_decode(html_entity_decode($params));
 	    if ($data != null) {
-		$this->bank_act = $data->bank_act;
+		$this->gl_accounts = $data->gl_accounts;
+		$this->convert = $data->convert;
 		$this->graph_type = $data->graph_type;
 		if ($data->weeks_past != '')
 		    $this->weeks_past = $data->weeks_past;
 		if ($data->weeks_future != '')
 		    $this->weeks_future = $data->weeks_future;
 		$this->by_month = $data->by_month;
+		$this->display_total = $data->display_total;
 	    }
 	}
     }
@@ -59,23 +63,25 @@ function week_of_month($date) {
 		}
 				
 
-		$basic_sql = "SELECT -sum(amount) AS amount,".
+		$basic_sql = "SELECT sum(amount) AS amount,".
 			($this->by_month ?
 			  "ADDDATE(SUBDATE(tran_date, DAYOFMONTH(tran_date)-1), INTERVAL  ".(-$offset)." MONTH )"
 				
 			 : "ADDDATE(SUBDATE(tran_date, weekday(tran_date)), ".-($offset*7)." )")
 		." AS trans_date
 					FROM ".TB_PREF.$table."
-					WHERE account IN (4000)";
-		$sql = $basic_sql . " AND (tran_date <= '$from_sql')";
-		$result = db_query($sql);
-		$r = db_fetch_assoc($result);
-		//$rows[]= array('trans_date' => $to_sql, 'amount' => $r['amount'] , 'type' => $type);
+					WHERE account IN (".$this->gl_accounts.")";
+		if($this->display_total) {
+			$sql = $basic_sql . " AND (tran_date <= '$from_sql')";
+			$result = db_query($sql);
+			$r = db_fetch_assoc($result);
+			$rows[]= array('trans_date' => $from_sql, 'amount' => $this->convert*$r['amount'] , 'type' => $type);
+		}
 
 		$sql = $basic_sql." AND (tran_date > '$from_sql' AND tran_date < '$to_sql') GROUP BY trans_date";
 	$result = db_query($sql);
 	while($r = db_fetch_assoc($result)) {
-			$rows[]= array('trans_date' => $r['trans_date'], 'amount' => $r['amount'] , 'type' => $type);
+			$rows[]= array('trans_date' => $r['trans_date'], 'amount' => $this->convert*$r['amount'] , 'type' => $type);
 		}
 
 	}
@@ -100,6 +106,12 @@ function week_of_month($date) {
 	$to = add_days($today, $this->weeks_future*7);
 	}
 
+	if (!isset($this->gl_accounts)) {
+		$this->gl_accounts = 4000;
+	}
+	if (!isset($this->convert)) { $this->convert = -1;
+	}
+
 	$transactions = array();
 		$this->get_account_transaction($from, $to, $transactions, 'transaction');
 		$this->get_account_transaction($from, $to, $transactions, 'previous', -($this->by_month ? 12 : 52));
@@ -111,17 +123,28 @@ function week_of_month($date) {
 	//flag is not needed
 	$flag = true;
 	$table = array();
-	$table['cols'] = array(
-	    array('label' => 'Date', 'type' => 'string'),
-	    //array('label' => 'Balance', 'type' => 'number'),
-	    array('label' => 'Transaction', 'type' => 'number'),
-	    array('label' => 'Previous Year', 'type' => 'number')
-	    ,array('label' => 'Budget', 'type' => 'number')
-	);
+
+	if($this->display_total) {
+		$table['cols'] = array(
+			array('label' => 'Date', 'type' => 'string'),
+			array('label' => 'Balance', 'type' => 'number'),
+			array('label' => 'Previous Year', 'type' => 'number')
+			,array('label' => 'Budget', 'type' => 'number')
+		);
+	}
+	else {
+		$table['cols'] = array(
+			array('label' => 'Date', 'type' => 'string'),
+			array('label' => 'Transaction', 'type' => 'number'),
+			array('label' => 'Previous Year', 'type' => 'number')
+			,array('label' => 'Budget', 'type' => 'number')
+		);
+	}
 
 	// We group all transactions by type
 	$rows = array();
 	$total = 0;
+	$previous_total;
 	$transaction = 0;
 	$smooth_transaction = 0;
 	$previous = 0;
@@ -130,64 +153,76 @@ function week_of_month($date) {
 	$last_day = 0;
 	//$date = nil;// $from; //add_days(Today(), -$this->weeks_past);
 	$balance_date = nil;// $date;
-		$i=0;
+	$i=0;
 	while($r = $transactions[$i]) {
-	    if ($r['trans_date'] == null) {
-		$total = $r['amount'];
-	    } else {
+		if ($r['trans_date'] == null) {
+			$total = $r['amount'];
+		} else {
 
-		$balance_date = sql2date($r['trans_date']);
-		if(!isset($date)) $date = $balance_date;
-		while (date1_greater_date2 ($balance_date, $date) ) {
-		    $temp = array();
-		    $temp[] = array('v' => (string) $date, 'f' => $date);
-		    //$temp[] = array('v' => (float) $total, 'f' => number_format2($total, user_price_dec()));
-		    $temp[] = array('v' => (float) $transaction, 'f' => number_format2($transaction, user_price_dec()));
-		    $temp[] = array('v' => (float) $previous, 'f' => number_format2($previous, user_price_dec()));
-		    $temp[] = array('v' => (float) $week_budget, 'f' => number_format2($week_budget, user_price_dec()));
-		    $rows[] = array('c' => $temp);
-		    if($this->by_month)
-			    $date = add_months($date,1);
-		    else
-			    $date = add_days($date,7);
+			$balance_date = sql2date($r['trans_date']);
+			if(!isset($date)) $date = $balance_date;
+			while (date1_greater_date2 ($balance_date, $date) ) {
+				$temp = array();
+				$temp[] = array('v' => (string) $date, 'f' => $date);
+				if($this->display_total)  {
+					$temp[] = array('v' => (float) $total, 'f' => number_format2($total, user_price_dec()));
+					$temp[] = array('v' => (float) $previous_total, 'f' => number_format2($previous_total, user_price_dec()));
+				}
+				else {
+					$temp[] = array('v' => (float) $transaction, 'f' => number_format2($transaction, user_price_dec()));
+					$temp[] = array('v' => (float) $previous, 'f' => number_format2($previous, user_price_dec()));
+				}
+				$temp[] = array('v' => (float) $week_budget, 'f' => number_format2($week_budget, user_price_dec()));
+				$rows[] = array('c' => $temp);
+				if($this->by_month)
+					$date = add_months($date,1);
+				else
+					$date = add_days($date,7);
 
-			$transaction = 0;
-			$previous = 0;
-			$budget -= $week_budget;
-			//$week_budget = min($budget, $week_budget);
+				$transaction = 0;
+				$previous = 0;
+				$budget -= $week_budget;
+				//$week_budget = min($budget, $week_budget);
+			}
+			$temp = array();
+			switch($r['type']) {
+			case 'transaction':
+				$total += $r['amount'];
+				$transaction = $r['amount'];
+				break;
+			case 'previous':
+				$previous_total += $r['amount'];
+				$previous = $r['amount'];
+				break;
+			case 'budget':
+				$total += $r['amount'];
+				$budget = $r['amount'];
+				$week_budget = $budget/$this->week_of_month($date);
+				break;
+			}
 		}
-		$temp = array();
-		switch($r['type']) {
-		case 'transaction':
-			$total += $r['amount'];
-			$transaction = $r['amount'];
-			break;
-		case 'previous':
-			$total += $r['amount'];
-			$previous = $r['amount'];
-			break;
-		case 'budget':
-			$total += $r['amount'];
-			$budget = $r['amount'];
-			$week_budget = $budget/$this->week_of_month($date);
-			break;
-		}
-	    }
-			$i+=1;
+		$i+=1;
 	}
 	$temp[] = array('v' => (string) $balance_date, 'f' => $balance_date);
-	//$temp[] = array('v' => (float) $total, 'f' => number_format2($total, user_price_dec()));
-	    $temp[] = array('v' => (float) $transaction, 'f' => number_format2($transaction, user_price_dec()));
-	    $temp[] = array('v' => (float) $previous, 'f' => number_format2($previous, user_price_dec()));
-	    $temp[] = array('v' => (float) $week_budget, 'f' => number_format2($week_budget, user_price_dec()));
-			$budget -= $week_budget;
-			//$week_budget = min($budget, $week_budget);
+				if($this->display_total)  {
+					$temp[] = array('v' => (float) $total, 'f' => number_format2($total, user_price_dec()));
+					$temp[] = array('v' => (float) $previous_total, 'f' => number_format2($previous_total, user_price_dec()));
+				}
+				else {
+					$temp[] = array('v' => (float) $transaction, 'f' => number_format2($transaction, user_price_dec()));
+					$temp[] = array('v' => (float) $previous, 'f' => number_format2($previous, user_price_dec()));
+				}
+	$temp[] = array('v' => (float) $transaction, 'f' => number_format2($transaction, user_price_dec()));
+	$temp[] = array('v' => (float) $previous, 'f' => number_format2($previous, user_price_dec()));
+	$temp[] = array('v' => (float) $week_budget, 'f' => number_format2($week_budget, user_price_dec()));
+	$budget -= $week_budget;
+	//$week_budget = min($budget, $week_budget);
 	$rows[] = array('c' => $temp);
 	$date = $balance_date;
-	    if($this->by_month)
-		    $date = add_months($date,1);
-	    else
-		    $date = add_days($date,7);
+	if($this->by_month)
+		$date = add_months($date,1);
+	else
+		$date = add_days($date,7);
 /*
 	$end_date = $to;
 	while (date1_greater_date2 ($end_date, $date)) {
@@ -220,67 +255,77 @@ function week_of_month($date) {
 		if ($this->graph_type == 'Table') {
 			$js .= "sortAscending: false, sortColumn: 0,  ";
 		}
-			$js .="height: 300, ";
+		$js .="height: 300, ";
 		$js .= "title: '".$title."'
 			,seriesType:'bars'
 			,series: {1: {type: 'bar', areaOpacity: 0.1}
-				, 2: {type: 'steppedArea', areaOpacity: 0.1}
-				, 0: {type: 'bar'}}
+			, 2: {type: 'steppedArea', areaOpacity: 0.1}
+			, 0: {type: 'bar'}}
 			, bar: {groupWidth: 20}
 			, isStacked: false
     };
-	var chart".$id." = new google.visualization.".$this->graph_type."(document.getElementById('widget_div_".$id."'));
-	chart".$id.".draw(data, options);
+    var chart".$id." = new google.visualization.".$this->graph_type."(document.getElementById('widget_div_".$id."'));
+    chart".$id.".draw(data, options);
     }";
-	add_js_source($js);
+    add_js_source($js);
     }
 
-	function edit_param()
-	{
-		$graph_types = array(
-			'ComboChart' => _("Chart"),
-			'Table' => _("Table")
-		);
-		$by_months = array(
-			0 => _("Week"),
-			1 => _("Month")
-		);
-		$_POST['weeks_past'] = $this->weeks_past;
-		$_POST['weeks_future'] = $this->weeks_future;
-		$_POST['bank_act'] = $this->bank_act;
-		$_POST['by_month'] = $this->by_month;
-		$_POST['graph_type'] = $this->graph_type;
-		text_row_ex(_("Weeks in past:"), 'weeks_past', 2);
-		text_row_ex(_("Weeks in future:"), 'weeks_future', 2);
-		//bank_accounts_list_cells(_("Account:"), 'bank_act', null);
-		select_row(_("Period"), "by_month", null, $by_months, null);
-		select_row(_("Graph Type"), "graph_type", null, $graph_types, null);
-	}
+    function edit_param()
+    {
+	    $graph_types = array(
+		    'ComboChart' => _("Chart"),
+		    'Table' => _("Table")
+	    );
+	    $by_months = array(
+		    0 => _("Week"),
+		    1 => _("Month")
+	    );
+	    $display_totals = array(
+		    0 => _("Transaction")
+		    ,1 => _("Total")
+	    );
+	    $_POST['weeks_past'] = $this->weeks_past;
+	    $_POST['weeks_future'] = $this->weeks_future;
+	    $_POST['gl_accounts'] = $this->gl_accounts;
+	    $_POST['convert'] = $this->convert;
+	    $_POST['by_month'] = $this->by_month;
+	    $_POST['graph_type'] = $this->graph_type;
+	    text_row_ex(_("Weeks in past:"), 'weeks_past', 2);
+	    text_row_ex(_("Weeks in future:"), 'weeks_future', 2);
+	    text_row_ex(_("Accounts"), 'gl_accounts', null);
+	    select_row(_("Invert"), 'convert', null, array(-1 => _("Yes"), 1=>_("No")), null);
+	    //bank_accounts_list_cells(_("Account:"), 'gl_accounts', null);
+	    select_row(_("Mode"), "display_total", null, $display_totals, null);
+	    select_row(_("Period"), "by_month", null, $by_months, null);
+	    select_row(_("Graph Type"), "graph_type", null, $graph_types, null);
+    }
 
-	function validate_param()
-	{
-		$input_error = 0;
-		//if (!is_numeric($_POST['top']))
-		//  {
-		//      $input_error = 1;
-		//      display_error( _("The number of weeks must be numeric."));
-		//      set_focus('top');
-		//  }
+    function validate_param()
+    {
+	    $input_error = 0;
+	    //if (!is_numeric($_POST['top']))
+	    //  {
+	    //      $input_error = 1;
+	    //      display_error( _("The number of weeks must be numeric."));
+	    //      set_focus('top');
+	    //  }
 
-		//  if ($_POST['top'] == '')
-		//      $_POST['top'] = 10;
+	    //  if ($_POST['top'] == '')
+	    //      $_POST['top'] = 10;
 
-		return $input_error;
-	}
+	    return $input_error;
+    }
 
-	function save_param()
-	{
-		$param = array('weeks_past' => $_POST['weeks_past'],
-			'weeks_future' => $_POST['weeks_future'],
-			'bank_act' => $_POST['bank_act'],
-			'graph_type' => $_POST['graph_type'],
-			'by_month' => $_POST['by_month']);
-		return json_encode($param);
-	}
+    function save_param()
+    {
+	    $param = array('weeks_past' => $_POST['weeks_past'],
+		    'weeks_future' => $_POST['weeks_future'],
+		    'gl_accounts' => $_POST['gl_accounts'],
+		    'convert' => $_POST['convert'],
+		    'graph_type' => $_POST['graph_type'],
+		    'display_total' => $_POST['display_total'],
+		    'by_month' => $_POST['by_month']);
+	    return json_encode($param);
+    }
 
 }
