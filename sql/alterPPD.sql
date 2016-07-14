b670cdc1923998d1d3cf224c0dbdc0188ff83ffe
Maxime Bourget <bmx007@gmail.com>
2016-07-14 09:28:40 +0100

    Displaying PPD in debtor transaction.
    PPD amount (and vat) are not stored yet in table.
    TAX needs to be displayed.

--- sql/alterPPD.sql
+++ sql/alterPPD.sql
@@ -1,7 +1,7 @@
 -- Add Prompt Payment Price to order 
 -- All default value should be set in the alter script
 ALTER TABLE `0_sales_order_details`
-ADD COLUMN `ppd` double NOT NULL DEFAULT 0 COMMENT 'unit price if PPD'
+ADD COLUMN `ppd` double NOT NULL DEFAULT 0 COMMENT 'Prompt payment DISCOUNT (fractional)"
 ;
 
 ALTER TABLE `0_sales_orders`
@@ -9,11 +9,11 @@ ADD COLUMN total_ppd double COMMENT 'total amount if PPD'
 ;
 
 ALTER TABlE `0_debtor_trans_details`
-ADD COLUMN `ppd` double NOT NULL DEFAULT 0 COMMENT 'unit price if PPD'
-,ADD COLUMN `ppd_gst` double COMMENT 'tax per unit if PPD'
+ADD COLUMN `ppd` double NOT NULL DEFAULT 0 COMMENT 'Prompt payment DISCOUNT (0 if not)""
+,ADD COLUMN `ppd_gst` double COMMENT 'tax deduction if PPD'
 ;
 
 ALTER TABLE `0_debtor_trans`
-ADD COLUMN `ov_ppd_amount` double COMMENT 'Net amount if PPD'
-,ADD COLUMN `ov_ppd_gst` double COMMENT 'Tax amount if PPD'
+ADD COLUMN `ov_ppd_amount` double COMMENT 'Net discount if PPD'
+,ADD COLUMN `ov_ppd_gst` double COMMENT 'Tax discount if PPD'
 ;
