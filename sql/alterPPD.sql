e4848ae0f4ccb0f7e4dba0bc04f8923c520f0fcd
Maxime Bourget <bmx007@gmail.com>
2016-07-17 23:08:29 +0100

    Uncomment sales_order_details alteration.

--- sql/alterPPD.sql
+++ sql/alterPPD.sql
@@ -1,8 +1,8 @@
 -- Add Prompt Payment Price to order 
 -- All default value should be set in the alter script
--- ALTER TABLE `0_sales_order_details`
--- ADD COLUMN `ppd` double NOT NULL DEFAULT 0 COMMENT 'Prompt payment DISCOUNT (fractional)'
--- ;
+ALTER TABLE `0_sales_order_details`
+ADD COLUMN `ppd` double NOT NULL DEFAULT 0 COMMENT 'Prompt payment DISCOUNT (fractional)'
+;
 
 ALTER TABLE `0_sales_orders`
 ADD COLUMN total_ppd double COMMENT 'total amount if PPD'
