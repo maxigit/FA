5711ef07e0a2d3acc47e06a130add78fee8d2cff
Maxime Bourget <bmx007@gmail.com>
2016-08-16 10:22:03 +0100

    Fix total PPD wrong on interactive invoice.

--- sales/customer_invoice.php
+++ sales/customer_invoice.php
@@ -613,7 +613,7 @@ $tax_totals = display_edit_tax_items($taxes, $colspan, $_SESSION['Items']->tax_i
 $tax_total = $tax_totals['total'];
 $tax_total_ppd = $tax_totals['totalWithPPD'];
 $display_total = price_format(($inv_items_total + input_num('ChargeFreightCost') + $tax_total));
-$display_total_ppd = inv_items_total_ppd + input_num('ChargeFreightCost') + $tax_total_ppd;
+$display_total_ppd = $inv_items_total_ppd + input_num('ChargeFreightCost') + $tax_total_ppd;
 
 start_row();
 label_cells(_("Invoice Total"), $display_total, "colspan=$colspan align=right","align=right", $is_batch_invoice ? 2 : 0);
